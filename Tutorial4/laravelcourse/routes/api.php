229b31b7da700d8917939002bc3c091fd7b6687c
mvasqueze <68928428+mvasqueze@users.noreply.github.com>
2023-04-18 22:25:49 -0500

    idk

--- Tutorial4/laravelcourse/routes/api.php
+++ Tutorial4/laravelcourse/routes/api.php
@@ -16,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+    Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
+    Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show')->name('api.product.show');
 });
